4186c32b292be3eae9a017b5cec1e539a7408a21
alvaresc <alvaresc@rose-hulman.edu>
2021-08-01 21:13:14 -0400

    adding title slides

--- Arduino/DeltaZ_communication/DeltaZ_communication.ino
+++ Arduino/DeltaZ_communication/DeltaZ_communication.ino
@@ -44,13 +44,13 @@ void loop() {
 //  myDelta.goTo(0, 0, height);
 
 // Uncomment below for joystick analog input
-  analogX = avgAnalogFast(xPin);
-  xVal = map(analogX, 0, 1022, -21, 21);
-  analogY = avgAnalogFast(yPin);
-  yVal = map(analogY, 0, 1022, -21, 21);
-  analogZ = avgAnalogFast(zPin);  // only uncomment this line if using potnetiometer with joystick
-  zVal = map(analogZ, 0, 1022, -75, -35); // only uncomment this line if using potnetiometer with joystick
-  myDelta.goTo(xVal,yVal,zVal);
+//  analogX = avgAnalogFast(xPin);
+//  xVal = map(analogX, 0, 1022, -21, 21);
+//  analogY = avgAnalogFast(yPin);
+//  yVal = map(analogY, 0, 1022, -21, 21);
+//  analogZ = avgAnalogFast(zPin);  // only uncomment this line if using potnetiometer with joystick
+//  zVal = map(analogZ, 0, 1022, -75, -35); // only uncomment this line if using potnetiometer with joystick
+//  myDelta.goTo(xVal,yVal,zVal);
 
 //  Uncomment for Ultrasonic Sensor
 //  distance = avg5US();
